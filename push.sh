9cc22a5e1a2b5f110775e8a0e5c7dc4e38fbabe8
reigadegr <2722688642@qq.com>
2023-07-20 20:09:05 +0800

    小幅度调整

--- push.sh
+++ push.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 #cd /data/data/com.termux/files/usr/bin
+/data/data/com.termux/files/usr/bin/git gc --prune=all
 /data/data/com.termux/files/usr/bin/git add .
 /data/data/com.termux/files/usr/bin/git commit -m "$1"
 /data/data/com.termux/files/usr/bin/git push
