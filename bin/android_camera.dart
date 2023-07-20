e3eb733d44521f51ee7a1f28cb31f0eef74ee712
reigadegr <2722688642@qq.com>
2023-07-20 20:01:23 +0800

    初始化项目

---
+++ bin/android_camera.dart
@@ -0,0 +1,5 @@
+import 'package:android_camera/android_camera.dart' as android_camera;
+
+void main(List<String> arguments) {
+  print('Hello world: ${android_camera.calculate()}!');
+}
---
+++ test/android_camera_test.dart
@@ -0,0 +1,8 @@
+import 'package:android_camera/android_camera.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
